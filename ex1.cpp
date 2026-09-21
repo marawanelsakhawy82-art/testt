8219d37a7acbef0ca9a1bc02df3b957fdbb2754c
marawanelsakhawy82-art <marawanelsakhawy82@gmail.com>
2026-09-22 00:23:18 +0300

    ffgfm

---
+++ ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int num1,num2,num3;
+    
+// where the user write the value 
+ cin >> num1 >> num2 >> num3;
+
+// where the is equation
+double sum= (num1 + num2+ num3)/3;
+
+cout << "average= " << sum << endl;
+    return 0;
+}
